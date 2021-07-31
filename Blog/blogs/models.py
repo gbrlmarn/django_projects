55b9ab4fb24d717b041064dccf90e325225680fb
gbrlmarn <gbrlmarn@gmail.com>
2021-07-31 19:19:55 +0300

    Added data restriction

--- Blog/blogs/models.py
+++ Blog/blogs/models.py
@@ -1,6 +1,7 @@
 import dataclasses
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -8,6 +9,7 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=50)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.BlogPost
--- Blog/blogs/views.py
+++ Blog/blogs/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 from .models import BlogPost
 from .forms import BlogPostForm
@@ -9,6 +11,7 @@ def index(request):
     context = {'blogposts' : blogposts}
     return render(request, 'blogs/index.html', context)
 
+@login_required
 def new_blogpost(request):
     """Add a new blogpost"""
     if request.method != 'POST':
@@ -16,14 +19,19 @@ def new_blogpost(request):
     else:
         form = BlogPostForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_blogpost = form.save(commit=False)
+            new_blogpost.owner = request.user
+            new_blogpost.save()
             return redirect('blogs:index')
 
     context = {'form' : form}
     return render(request, 'blogs/new_blogpost.html', context)
 
+@login_required
 def edit_blogpost(request, blogpost_id):
     blogpost = BlogPost.objects.get(id=blogpost_id)
+    if blogpost.owner != request.user:
+        raise Http404
 
     if request.method != 'POST':
         form = BlogPostForm(instance=blogpost)
